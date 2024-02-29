0b3a25a04953a778c99908f46820e36f4f18a02f
Mateus Gomes <mateus.gs1999@gmail.com>
2024-02-29 13:54:37 -0300

    adicionado rota para resultado do pedido

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Order } from "./pages/Order";
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { ResultOrder } from "./pages/ResultOrder";
 
 export function Router() {
     return (
@@ -9,6 +10,7 @@ export function Router() {
             <Route path="/" element={<DefaultLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/pedido" element={<Order />} />
+                <Route path="/pedido-confirmado" element={<ResultOrder />} />
             </Route>
         </Routes>
     )
