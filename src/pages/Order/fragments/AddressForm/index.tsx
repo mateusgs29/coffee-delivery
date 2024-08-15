c3fcc111cdef5e77a6ddf843e30f4ec21655f630
Mateus Gomes <mateus.gs1999@gmail.com>
2024-08-15 18:32:29 -0300

    eventos controlados

--- src/pages/Order/fragments/AddressForm/index.tsx
+++ src/pages/Order/fragments/AddressForm/index.tsx
@@ -1,14 +1,24 @@
 import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
 import { Title } from "../../styles";
 import { CardContainer, FormAddress, FormPayment, HeaderCard, InfoCard, InputOptional, InputRadio, InputText } from "./styles";
+import { useFormContext } from "react-hook-form";
 
 export function AddressForm() {
+    const { register, setValue } = useFormContext();
+
+    const handleCepChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const rawValue = event.target.value.replace(/\D/g, '');
+        const formattedValue = rawValue.replace(/(\d{5})(\d{1,3})/, '$1-$2');
+    
+        setValue('cep', formattedValue, { shouldValidate: true });
+    }
+
     return (
         <div>
             <Title>Complete seu pedido</Title>
 
             <CardContainer>
-                <HeaderCard colorIcon="yellow">
+                <HeaderCard coloricon="yellow">
                     <MapPinLine size={25} />
                     
                     <InfoCard>
@@ -18,21 +28,65 @@ export function AddressForm() {
                 </HeaderCard>
 
                 <FormAddress>
-                    <InputText gridArea="cep" type="text" placeholder="CEP" />
-                    <InputText gridArea="rua" type="text" placeholder="Rua" />
-                    <InputText gridArea="numero" type="text" placeholder="Número" />
+                    <InputText 
+                        gridarea="cep" 
+                        type="text" 
+                        placeholder="CEP"
+                        {...register('cep', {
+                            required: 'Campo obrigatório',
+                            pattern: {
+                                value: /^[0-9]{5}-[0-9]{3}$/,
+                                message: 'CEP inválido'
+                            }
+                        })}
+                        onChange={handleCepChange}
+                        maxLength={9}
+                    />
+                    <InputText 
+                        gridarea="rua" 
+                        type="text" 
+                        placeholder="Rua"
+                        {...register('rua')}
+                    />
+                    <InputText 
+                        gridarea="numero" 
+                        type="text" 
+                        placeholder="Número"
+                        {...register('numero')} 
+                    />
                     <InputOptional>
-                        <InputText gridArea="complemento" type="text" placeholder="Complemento" />
+                        <InputText 
+                            gridarea="complemento" 
+                            type="text" 
+                            placeholder="Complemento" 
+                            {...register('complemento')}
+                        />
                         <span>Opcional</span>
                     </InputOptional>
-                    <InputText gridArea="bairro" type="text" placeholder="Bairro" />
-                    <InputText gridArea="cidade" type="text" placeholder="Cidade" />
-                    <InputText gridArea="uf" type="text" placeholder="UF" />
+                    <InputText 
+                        gridarea="bairro" 
+                        type="text" 
+                        placeholder="Bairro"
+                        {...register('bairro')} 
+                    />
+                    <InputText 
+                        gridarea="cidade" 
+                        type="text" 
+                        placeholder="Cidade"
+                        {...register('cidade')} 
+                    />
+                    <InputText 
+                        gridarea="uf" 
+                        type="text" 
+                        placeholder="UF"
+                        {...register('uf')}
+                        maxLength={2}
+                    />
                 </FormAddress>
             </CardContainer>
 
             <CardContainer>
-                 <HeaderCard colorIcon="purple">
+                 <HeaderCard coloricon="purple">
                     <CurrencyDollar size={25} />
 
                     <InfoCard>
@@ -46,8 +100,8 @@ export function AddressForm() {
                             <input 
                                 type="radio" 
                                 id="credito" 
-                                name="pagamento" 
-                                value="credito" 
+                                value="credito"
+                                {...register('pagamento')}
                             />
                             <label htmlFor="credito">
                                 <CreditCard size={20} />
@@ -57,9 +111,9 @@ export function AddressForm() {
                         <InputRadio>
                             <input 
                                 type="radio" 
-                                id="debito"
-                                name="pagamento" 
-                                value="debito" 
+                                id="debito" 
+                                value="debito"
+                                {...register('pagamento')} 
                             />
                             <label htmlFor="debito">
                                 <Bank size={20} />
@@ -70,8 +124,8 @@ export function AddressForm() {
                             <input 
                                 type="radio" 
                                 id="dinheiro"
-                                name="pagamento" 
-                                value="dinheiro" 
+                                value="dinheiro"
+                                {...register('pagamento')}
                             />
                             <label htmlFor="dinheiro">
                                 <Money size={20} />
