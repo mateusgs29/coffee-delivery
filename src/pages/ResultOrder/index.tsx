dc2c641d15e8ee060103607c2bc6fd118400e1a7
Mateus Gomes <mateus.gs1999@gmail.com>
2024-08-15 18:33:51 -0300

    variaveis do context adicionado

--- src/pages/ResultOrder/index.tsx
+++ src/pages/ResultOrder/index.tsx
@@ -1,8 +1,18 @@
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import imgDeliveryOrder from '../../assets/img-delivery-order.png'
 import { Container, Content, IconList, ListResultOrder, Subtitle, TextsList, Title } from './styles'
+import { useContext } from 'react'
+import { CoffeesContext } from '../../context/CoffeesContext'
 
 export function ResultOrder() {
+    const { address, payment } = useContext(CoffeesContext)
+
+    const paymentLabel: { [key: string]: string } = {
+        'credito': 'Cartão de Crédito',
+        'debito': 'Cartão de Débito',
+        'dinheiro': 'Dinheiro'
+    }
+
     return (
         <Container>
             <Title>Uhu! Pedido confirmado</Title>
@@ -11,15 +21,15 @@ export function ResultOrder() {
             <Content>
                 <ListResultOrder>
                     <li>
-                        <IconList colorIcon='purple'><MapPin size={16} color="white" weight='fill' /></IconList>
+                        <IconList coloricon='purple'><MapPin size={16} color="white" weight='fill' /></IconList>
                         <TextsList>
-                            <span>Entrega em <strong>Rua João Daniel Martinelli, 103</strong></span>
-                            <span>Farrapos - Porto Alegre, RS</span>
+                            <span>Entrega em <strong>{ address.rua }, { address.numero }</strong></span>
+                            <span>{ address.bairro } - { address.cidade}, { address.uf }</span>
                         </TextsList>
                     </li>
 
                     <li>
-                        <IconList colorIcon='yellow'><Timer size={16} color="white" weight='fill' /></IconList>
+                        <IconList coloricon='yellow'><Timer size={16} color="white" weight='fill' /></IconList>
                         <TextsList>
                             <span>Previsão de entrega</span>
                             <span><strong>20 min - 30 min</strong></span>
@@ -27,10 +37,10 @@ export function ResultOrder() {
                     </li>
 
                     <li>
-                        <IconList colorIcon='yellow-dark'><CurrencyDollar size={16} color="white" weight='fill' /></IconList>
+                        <IconList coloricon='yellow-dark'><CurrencyDollar size={16} color="white" weight='fill' /></IconList>
                         <TextsList>
                             <span>Pagamento na entrega</span>
-                            <span><strong>Dinheiro</strong></span>
+                            <span><strong>{ paymentLabel[payment] }</strong></span>
                         </TextsList>
                     </li>
                 </ListResultOrder>
