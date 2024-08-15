ddc09ebc70ccba06c73060e2a8f76ea68a8efd56
Mateus Gomes <mateus.gs1999@gmail.com>
2024-08-15 15:08:41 -0300

    provider adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+import { CoffeesContextProvider } from "./context/CoffeesContext";
 
 export function App() {
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<BrowserRouter>
+				<CoffeesContextProvider>
+					<Router />
+				</CoffeesContextProvider>
+			</BrowserRouter>
       
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+			<GlobalStyle />
+		</ThemeProvider>
+	)
 }
